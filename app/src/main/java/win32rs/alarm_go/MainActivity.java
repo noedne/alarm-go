63b67382834f683e1251b549b4ce124b54c9612e
maximusjohnsonus <maximusjohnsonus@gmail.com>
2016-09-17 01:05:50 -0400

    Forcefully borrowed other people's code for alarm

--- app/src/main/java/win32rs/alarm_go/MainActivity.java
+++ app/src/main/java/win32rs/alarm_go/MainActivity.java
@@ -1,7 +1,10 @@
 package win32rs.alarm_go;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
     }
 }
